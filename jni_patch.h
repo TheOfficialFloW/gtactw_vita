172d504b084aa6391c517b01e120d5a20ad15ac6
Andy Nguyen <theofficialflow1996@gmail.com>
2021-03-17 11:50:33 +0100

    Remove old files.

--- jni_patch.h
+++
@@ -1,8 +0,0 @@
-#ifndef __JNI_PATCH_H__
-#define __JNI_PATCH_H__
-
-void *NVThreadGetCurrentJNIEnv(void);
-
-void jni_load(void);
-
-#endif
--- openal_patch.h
+++
@@ -1,6 +0,0 @@
-#ifndef __OPENAL_PATCH_H__
-#define __OPENAL_PATCH_H__
-
-void patch_openal(void);
-
-#endif
